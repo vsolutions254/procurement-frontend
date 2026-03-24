05991fbaf5b11158c74a6d8d6724c8920e04efee
chrisdroid <bobitlmr188221@spu.ac.ke>
2026-03-24 23:37:41 +0300

    Set up recommended items slice

--- lib/redux/store.ts
+++ lib/redux/store.ts
@@ -1,4 +1,8 @@
-import { configureStore, type ThunkDispatch, type UnknownAction } from "@reduxjs/toolkit";
+import {
+  configureStore,
+  type ThunkDispatch,
+  type UnknownAction,
+} from "@reduxjs/toolkit";
 import authReducer from "./features/auth/authSlice";
 import merchantsReducer from "./features/merchants/merchantSlice";
 import productsReducer from "./features/products/productsSlice";
@@ -7,6 +11,7 @@ import servicesCartReducer from "./features/services/cart/cartSlice";
 import servicesReducer from "./features/services/servicesSlice";
 import productCategoriesReducer from "./features/products/categories/categoriesSlice";
 import serviceCategoriesReducer from "./features/services/categories/serviceCategoriesSlice";
+import recommendedItemsReducer from "./features/recommended-items/recommendedItemsSlice";
 import suppliersReducer from "./features/suppliers/supplierSlice";
 
 export const makeStore = () => {
@@ -20,6 +25,7 @@ export const makeStore = () => {
       services: servicesReducer,
       product_categories: productCategoriesReducer,
       service_categories: serviceCategoriesReducer,
+      recommended_items: recommendedItemsReducer,
       suppliers: suppliersReducer,
     },
   });
